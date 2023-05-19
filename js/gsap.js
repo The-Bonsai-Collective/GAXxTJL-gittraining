e0d74f845c300ee863d29ff20e3a7f7325a7e6eb
Ben Ervine <gakdesign@gmail.com>
2023-05-19 13:59:02 +0100

    GSAP

--- js/gsap.js
+++ js/gsap.js
@@ -2,30 +2,31 @@
 	$(document).ready(function() {
 
 // GSAP Initial
-console.clear();
-gsap.registerPlugin(ScrollTrigger);
+		console.clear();
+		gsap.registerPlugin(ScrollTrigger);
+
 
 
 gsap.to(".text-block h2", {
   scrollTrigger: {
     trigger: ".pin-this",
     markers: true,
-    start: "top bottom",
-    end: "bottom -300%",
+    start: "top top",
+    end: "+=1000",
     pin: ".text-block h2",
     scrub: 2,
   },
 });
 gsap.to(".image-block img", {
   scale: 1.5,
-  ease: "power2",
+  ease: "none",
   force3D: true,
   scrollTrigger: {
     scale: 2,
     trigger: ".pin-this",
     markers: true,
-    start: "top bottom",
-    end: "bottom -300%",
+    start: "top top",
+    end: "+=1000",
     pin: ".image-block",
     scrub: 2,
   },
