8a1f719a1f1044659571a3b15eb92eaee873d26b
Ben Ervine <gakdesign@gmail.com>
2023-05-19 13:36:30 +0100

    probs broke it all

--- js/gsap.js
+++ js/gsap.js
@@ -6,52 +6,33 @@
 		gsap.registerPlugin(ScrollTrigger);
 
 
-var texts = gsap.utils.toArray(".sticky-content .text");
-texts.forEach((text, i) => {
-  ScrollTrigger.create({
-    pin: text,
-    start: "top 40%",
-    trigger: jQuery(text).parent().parent(),
-    endTrigger: jQuery(text).parent().parent(),
-    end: "bottom 40%+=" + gsap.getProperty(text, "height"),
-    //pinType: isTouch ? "fixed" : "transform",
+
+
+gsap.to(".text-block", {
+  scrollTrigger: {
+    trigger: ".pin-this",
     markers: true,
-    refreshPriority: -1
-  });
+    start: "top top",
+    end: "+=400",
+    pin: ".text-block",
+    scrub: 2,
+  },
 });
-
-var images = gsap.utils.toArray(".zoomer .images");
-images.forEach((image, i) => {
-  gsap.fromTo(
-    image,
-    { scale: 0.5 },
-    {
-      scale: 1.5,
-      ease: "none",
-      force3D: true,
-      scrollTrigger: {
-        pin:jQuery(image).parent(),
-        trigger: jQuery(image).parent(),
-        start: "top top",
-        end: "bottom top",
-        //pinType: isTouch ? "fixed" : "transform",
-        scrub: 0.5,
-        markers: true
-      }
-    }
-  );
+gsap.to(".image-block", {
+  scale: 1.5,
+  ease: "none",
+  force3D: true,
+  scrollTrigger: {
+    scale: 2,
+    trigger: ".pin-this",
+    markers: true,
+    start: "top top",
+    end: "+=400",
+    pin: ".image-block",
+    scrub: 2,
+  },
+  
 });
-
-let st = ScrollTrigger.create({
-        trigger: ".pin-this",
-        pin: ".text-block",
-        start: "top center",
-        end: "+=500"
-      });
-
-console.log(st.pin);
-
-
 
 
 
