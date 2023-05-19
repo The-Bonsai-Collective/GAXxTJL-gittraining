521597f8866205a33491c8ce99238571e2f4198b
Ben Ervine <gakdesign@gmail.com>
2023-05-19 12:32:19 +0100

    EXPANING !

--- js/gsap.js
+++ js/gsap.js
@@ -6,59 +6,31 @@
 		gsap.registerPlugin(ScrollTrigger);
 
 
-const isTouch = "ontouchstart" in document.documentElement;
+// --- ORANGE PANEL ---
 
-var texts = gsap.utils.toArray(".sticky-content .text");
-texts.forEach((text, i) => {
-  ScrollTrigger.create({
-    pin: text,
-    start: "top 0%",
-    trigger: jQuery(text).parent().parent(),
-    endTrigger: jQuery(text).parent().parent(),
-    end: "bottom 0%+=" + gsap.getProperty(text, "height"),
-    //pinType: isTouch ? "fixed" : "transform",
-    markers: true,
-    refreshPriority: -1
-  });
-});
-
-var images = gsap.utils.toArray(".sticky-content .zoomer .images");
-images.forEach((image, i) => {
-  gsap.fromTo(
-    image,
-    { scale: 0.5 },
-    {
-      scale: 1,
-      ease: "none",
-      force3D: true,
-      scrollTrigger: {
-        pin:jQuery(image).parent(),
-        trigger: jQuery(image).parent(),
-        start: "top top",
-        end: "bottom top",
-        //pinType: isTouch ? "fixed" : "transform",
-        scrub: 0.5,
-        markers: true
-      }
-    }
-  );
-});
-
-//ScrollTrigger.sort();
-
-
-
-
-var largeTL = gsap.timeline({
-  scrollTrigger: { 
-    trigger: '.start-trig',
-    pin: '.bg', 
-    pinSpacing: false,
-    start: "top top",
-    markers: true,
-    end: '+=200%',
-  },
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".orangeWrapper",
+    scrub: true,
+    pin: true,
+    
+    start: "50% 50%",
+    end: "+=200%"
+  }
 })
+
+.from(".orange", {
+  scale: 0.5, 
+  ease: "none"
+})
+
+.from(".line-2", {
+  scaleX:0,
+  ease:"none",
+  transformOrigin:"left top"
+}, 0)
+
+
 
 
 });
